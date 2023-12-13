796f80361ebbb9ac26d846c8da3b8b01f10bc0f5
Alae Eddine <59610755+RockYou-cmd@users.noreply.github.com>
2023-12-13 13:02:28 +0100

    added status to get chat

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { UserService } from 'src/user/user.service';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { UserService } from "src/user/user.service";
 
 const prisma = new PrismaClient();
 
@@ -33,7 +33,7 @@ export class ChatService {
         },
       });
       // console.log(updatedChat)
-      return 'message sent!';
+      return "message sent!";
     } catch (err) {
       throw err;
     }
@@ -41,27 +41,40 @@ export class ChatService {
 
   async getChat(account, user) {
     try {
-    //   await prisma.message.deleteMany();
-    //   await prisma.chat.deleteMany();
+      //   await prisma.message.deleteMany();
+      //   await prisma.chat.deleteMany();
       const chat = await prisma.user.findUnique({
         where: {
-			username: account.username
+          username: account.username,
+        },
+        select: {
+          chats: {
+            where: {
+              members: {
+                some: {
+                  username: user.username,
+                },
+              },
+            },
+            include: {
+              messages: true,
+            },
           },
-		  select: {
-			chats: {
-				where:{ 
-					members: {
-						some:{
-							username: user.username
-						}
-					}
-				},
-				include: {
-					messages: true
-				}
-			}
-		  }
+          friends: {
+            where: {
+              users: {
+                some: {
+                  username: user.username
+                }
+              },
+            },
+            select: {
+              status: true
+            }
+          },
+        },
       });
+      console.log({chat: chat.chats[0], status: chat.friends[0].status});
       return chat;
     } catch (err) {
       throw err;
