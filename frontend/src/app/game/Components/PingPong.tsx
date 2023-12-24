9bdc49397e0674f867f69a45edbb39d78b7f82b7
RockYou <elkorchi.alae@gmail.com>
2023-12-24 20:08:04 +0100

    updated y in the move event handler

--- frontend/src/app/game/Components/PingPong.tsx
+++ frontend/src/app/game/Components/PingPong.tsx
@@ -17,107 +17,164 @@ export default function PingPong({map, ballColor, paddleColor, PLAYER1, PLAYER2,
 	const [myScore, setMyScore] = useState(0);
 	const [oppScore, setOppScore] = useState(0);
 	const game = useRef<HTMLCanvasElement>(null);
-	
+	const roomName = useRef("");
 
+  useEffect(() => {
+    if (game.current === null) return;
 
-	useEffect(() => {
-		if (game.current === null)
-			return;
+    // let parent = game.current?.parentElement;
+    // game.current.width = parent?.clientWidth || 0;
+    // game.current.height = parent?.clientHeight || 0;
+    let gameWidth = game.current.width || 0;
+    let gameHeight = game.current.height || 0;
+    let player1 = {
+      x: 20,
+      y: gameHeight / 2 - 120 / 2,
+      score: 0,
+      username: PLAYER1,
+      width: 12,
+      height: 140,
+      color: paddleColor,
+    };
+    let player2 = {
+      x: gameWidth - 32,
+      y: gameHeight / 2 - 120 / 2,
+      score: 0,
+      username: PLAYER2,
+      width: 12,
+      height: 140,
+      color: paddleColor,
+    };
+    let ball = { x: 0, y: 0, color: ballColor, radius: 16 };
+    let net = {
+      x: gameWidth / 2 - 2 / 1,
+      y: 0,
+      width: 2,
+      height: 10,
+      color: "white",
+    };
+    const context = game.current?.getContext("2d");
 
-		// let parent = game.current?.parentElement;
-		// game.current.width = parent?.clientWidth || 0;
-		// game.current.height = parent?.clientHeight || 0;
-		let gameWidth = game.current.width || 0;
-		let gameHeight = game.current.height || 0;
-		let player1 = {x: 20, y: gameHeight / 2 - 120 /2 , score: 0, username: "", width: 12, height : 140, color : paddleColor};
-		let player2 = {x: gameWidth -32, y: gameHeight /2 -120 /2, score: 0, username: "", width: 12, height : 140, color : paddleColor};
-		let ball = {x: 0, y: 0, color : ballColor, radius : 16 };
-		let net={x: gameWidth/2 - 2/1, y: 0, width: 2, height: 10, color: "white"};
-		const context = game.current?.getContext('2d');
+    socket?.on("frame", (data: any) => {
+      player1.x = data.player1.x;
+      player1.y = data.player1.y;
+      player1.score = data.player1.score;
+      player2.x = data.player2.x;
+      player2.y = data.player2.y;
+      player2.score = data.player2.score;
+      ball.x = data.ball.x;
+      ball.y = data.ball.y;
+      roomName.current = data?.roomName;
+      if (myScore != data.player1.score) setMyScore(data.player1.score);
+      if (oppScore != data.player2.score) setOppScore(data.player2.score);
+      console.log("data", data);
+      render();
+    });
 
-		socket?.on("frame", (data : any) => {
-			player1.x = data.player1.x;
-			player1.y = data.player1.y;
-			player1.score = data.player1.score;
-			player2.x = data.player2.x;
-			player2.y = data.player2.y;
-			player2.score = data.player2.score;
-			ball.x = data.ball.x;
-			ball.y = data.ball.y;
-			if (myScore != data.player1.score)
-				setMyScore(data.player1.score);
-			if (oppScore != data.player2.score)
-				setOppScore(data.player2.score);
-			render();
-		})
+    socket?.on("endGame", (data: any) => {
+      close("");
+    });
 
-		socket?.on("endGame", (data : any) => {
-			close("");
-		});
+    function drawRect(
+      x: number,
+      y: number,
+      w: number,
+      h: number,
+      color: string
+    ) {
+      if (context) {
+        context.fillStyle = color;
+        context.fillRect(x, y, w, h);
+      }
+    }
 
-		function drawRect(x: number, y: number, w: number, h: number, color: string) {
-			if (context){
-				context.fillStyle = color;
-				context.fillRect(x, y, w, h);
-			}		
-		}
+    function drawCircle(x: number, y: number, r: number, color: string) {
+      if (context) {
+        context.fillStyle = color;
+        context.beginPath();
+        context.arc(x, y, r, 0, Math.PI * 2, false);
+        context.closePath();
+        context.fill();
+      }
+    }
 
-		function drawCircle(x: number, y: number, r: number, color: string) {
-			if (context){
-				context.fillStyle = color;
-				context.beginPath();
-				context.arc(x, y, r, 0, Math.PI * 2, false);
-				context.closePath();
-				context.fill();
-			}
-		}
+    function drawNet() {
+      for (let i = 0; i <= gameHeight; i += 15) {
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+      }
+    }
 
-		function drawNet() {
-			for (let i = 0; i <= gameHeight; i += 15) {
-				drawRect(net.x, net.y + i, net.width, net.height, net.color);
-			}
-		}
+    function render() {
+      context?.clearRect(0, 0, gameWidth, gameHeight);
 
-		function render() {
-			context?.clearRect(0, 0, gameWidth, gameHeight);
+      drawRect(0, 0, gameWidth, 6, "white");
+      drawRect(0, 0, 6, gameHeight, "white");
+      drawRect(0, gameHeight - 6, gameWidth, 6, "white");
+      drawRect(gameWidth - 6, 0, 6, gameHeight, "white");
 
-			drawRect(0, 0, gameWidth, 6, "white");
-			drawRect(0, 0, 6, gameHeight, "white");
-			drawRect(0, gameHeight - 6 , gameWidth, 6, "white");
-			drawRect(gameWidth - 6, 0, 6,gameHeight, "white");
-			
-			drawNet();
-			drawRect(player1.x, player1.y, player1.width, player1.height, player1.color);
-			drawRect(player2.x, player2.y, player2.width, player2.height, player2.color);
-			//draw the ball
-			drawCircle(ball.x, ball.y, ball.radius, ball.color);
-		}
-		
-		document.body.addEventListener("keydown", function(key){
-			if (key.code == "ArrowUp"){
-				if (player1.username == me?.username){
-					if (player1.y > 0)
-						socket?.emit("move", {y : player1.y - 20});
-				}
-				else{
-					if (player2.y > 0)
-						socket?.emit("move", {y : player2.y - 20});
-				}
-			}
-			else if(key.code == "ArrowDown"){
-				if (player2.username == me?.username){
-					if (player2.y + player2.height < gameHeight)
-						socket?.emit("move", {y : player2.y + 20});
-				}
-				else{
-					if (player1.y + player1.height < gameHeight)
-						socket?.emit("move", {y : player1.y + 20});
-				}
-			}
-		});
-		
-		return(() => {socket?.off("frame", ()=>{})})
-	},[socket])
+      drawNet();
+      drawRect(
+        player1.x,
+        player1.y,
+        player1.width,
+        player1.height,
+        player1.color
+      );
+      drawRect(
+        player2.x,
+        player2.y,
+        player2.width,
+        player2.height,
+        player2.color
+      );
+      //draw the ball
+      drawCircle(ball.x, ball.y, ball.radius, ball.color);
+    }
+
+    document.body.addEventListener("keydown", function (key) {
+      if (key.code == "ArrowUp") {
+        if (player1.username == me?.username) {
+          if (player1.y > 0)
+            socket?.emit("move", {
+              y: player1.y - 20,
+              roomName: roomName.current,
+              player: "player1",
+              player1: player1.username,
+            });
+        } else {
+          if (player2.y > 0)
+            socket?.emit("move", {
+              y: player2.y - 20,
+              roomName: roomName.current,
+              player: "player2",
+              player2: player2.username,
+            });
+        }
+      } else if (key.code == "ArrowDown") {
+        if (player2.username == me?.username) {
+          if (player2.y + player2.height < gameHeight)
+            socket?.emit("move", {
+              y: player2.y + 20,
+              roomName: roomName.current,
+              player: "player2",
+              player2: player2.username,
+            });
+        } else {
+          if (player1.y + player1.height < gameHeight)
+            socket?.emit("move", {
+              y: player1.y + 20,
+              roomName: roomName.current,
+              player: "player1",
+              player1: player1.username,
+            });
+        }
+      }
+    });
+
+    return () => {
+      socket?.off("frame", () => {});
+    };
+  }, [socket]);
 
 
 	
