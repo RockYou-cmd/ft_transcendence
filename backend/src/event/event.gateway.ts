41a9d9ee2de4ca3f24a4d87bb43dc48fb73d2441
Alae Eddine <59610755+RockYou-cmd@users.noreply.github.com>
2024-01-02 12:06:50 +0100

    hiding messages from the blocked friendship inside the rooms

--- backend/src/event/event.gateway.ts
+++ backend/src/event/event.gateway.ts
@@ -59,14 +59,14 @@ export class EventGateway {
       client.leave(username);
       var match = this.findMatch(username);
       if (match) {
-		  this.userService.updateData({ username }, { status: "ONLINE" });
-		  const player1 = Array.from(match.keys())[0];
-		  const player2 = Array.from(match.keys())[1];
-		  this.server
+        this.userService.updateData({ username }, { status: "ONLINE" });
+        const player1 = Array.from(match.keys())[0];
+				if (match.size == 4) var player2 = Array.from(match.keys())[1];
+        else var player2 = Array.from(match.keys())[2];
+        this.server
           .to(player1)
           .to(player2)
           .emit("endGame", "the opponent left");
-		  console.log(match);
         clearInterval(match.get("loop"));
         match?.get("game")?.reset();
         match.clear();
@@ -92,7 +92,10 @@ export class EventGateway {
     const userData = await this.roomService.getMemberShip(
       payload.chatId,
       payload.sender,
-    );
+		);
+		
+		var blockedBy = userData.user.blockedBy.map(user => user?.users[0]?.username);
+		// console.log(userData.user);
     if (userData.status == "MUTED") {
       const now = new Date();
       if (userData.mutedTime > now) {
@@ -104,13 +107,18 @@ export class EventGateway {
           username: userData.userId,
         });
     }
-    payload.receivers.forEach((receiver) => {
-      this.server
-        .to(receiver.userId)
-        .except(payload.sender)
-        .emit("roomMessage", payload);
+		payload.receivers.forEach((receiver) => {
+			// console.log(receiver.userId)
+			// console.log(blockedBy.includes(receiver.userId));
+      if (!blockedBy.includes(receiver.userId)) {
+        console.log("receiver : ", receiver.userId);
+        this.server
+          .to(receiver.userId)
+          .except(payload.sender)
+          .emit("roomMessage", payload);
+			}
     });
-    console.log("message Sent!");
+    // console.log("message Sent!");
     this.roomService.sendMessage(payload);
   }
 
