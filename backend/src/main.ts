9538496e7886fbe19f08a9ca76597e1092bf2d94
Alae Eddine <59610755+RockYou-cmd@users.noreply.github.com>
2024-01-10 22:51:47 +0100

    included gameProfile to getUserData

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:{credentials:true, origin: true}});
+  const app = await NestFactory.create(AppModule, {
+    cors: { credentials: true, origin: "http://localhost:3000" },
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser())
   await app.listen(3001);
